d6f85da282c0c5bccbbb13792e632cb85f595d4d
Gnezd <ycdzeng@gmail.com>
2023-06-06 17:41:43 +0800

    complete cleanup, properties reader

--- dat_plot.rb
+++ dat_plot.rb
@@ -1,13 +1,14 @@
 require 'pry'
 
-dats = Dir.glob './*.dat'
+pixelpath = './mock_datafile/0'
+dats = Dir.glob pixelpath+'/segments/0/*.dat'
 raw = File.open(dats[0], 'rb').read
 puts raw.size
 
 decode_scheme = 'N*'
 puts dats
 ars = dats.map{|file| File.open(file, 'rb').read.unpack(decode_scheme)}
-fout = File.open('out', 'w')
+fout = File.open('outputs/out', 'w')
 (0..ars[0].size-1).each do |i|
   fout.puts ((0..ars.size-1).map {|j| ars[j][i]}).join "\t"
 end
